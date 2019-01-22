ee69ecb0b04771370f97bd61dbf4481ee022b1f5
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-22 22:59:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,8 @@
 # Going 30 mph on the freeway would be unsafe, as would going 95 mph.
 # Going 50 miles per hour, however, would return false as that's within the "safe" range.
 def unsafe?(speed)
-
+	if speed > 60
+		return true
 end
 
 
