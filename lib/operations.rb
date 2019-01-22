44cb4dc07247e5e5f28e68cb5adbf96817a8d6df
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-22 22:59:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,10 +2,11 @@
 # It should return true if the speed is either below 40 or above 60.
 # Going 30 mph on the freeway would be unsafe, as would going 95 mph.
 # Going 50 miles per hour, however, would return false as that's within the "safe" range.
-def unsafe?(speed)
-	if speed > 60
-		return true
-end
+	def unsafe?(speed)
+		if speed > 60
+			return true
+		end
+	end
 
 
 
